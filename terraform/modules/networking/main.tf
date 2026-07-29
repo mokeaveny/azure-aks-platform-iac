0b91258233f7cc2cf97467bfda89e5be3caf7687
mokeaveny <michaelkeaveny0@gmail.com>
2026-07-29 20:54:46 +0100

    Added the networking module definition

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "aks_vnet" {
+    name = "${var.project_name}-vnet"
+    location = var.location
+    resource_group_name = var.resource_group_name
+    address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+    name = "${var.project_name}-subnet"
+    resource_group_name = var.resource_group_name
+    virtual_network_name = azurerm_virtual_network.aks_vnet.name
+    address_prefixes = ["10.0.1.0/24"]
+}
