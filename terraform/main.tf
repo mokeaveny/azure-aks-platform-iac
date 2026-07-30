13fde74f56498c7aabb98f043be284c89de63534
mokeaveny <michaelkeaveny0@gmail.com>
2026-07-30 23:01:23 +0100

    Recursive fix so that it has a consistent format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
 resource "azurerm_resource_group" "resource_group" {
-  name = "${var.project_name}-resources"
+  name     = "${var.project_name}-resources"
   location = var.location
 }
 
 module "networking" {
-  source = "./modules/networking"
-  project_name = var.project_name
-  location = var.location
+  source              = "./modules/networking"
+  project_name        = var.project_name
+  location            = var.location
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
 module "acr" {
-  source = "./modules/acr"
-  project_name = var.project_name
-  location = var.location
+  source              = "./modules/acr"
+  project_name        = var.project_name
+  location            = var.location
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
 module "aks" {
-  source = "./modules/aks"
-  project_name = var.project_name
-  location = var.location
+  source              = "./modules/aks"
+  project_name        = var.project_name
+  location            = var.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  environment = var.environment
-  subnet_id = module.networking.subnet_id
-  acr_id = module.acr.acr_id
+  environment         = var.environment
+  subnet_id           = module.networking.subnet_id
+  acr_id              = module.acr.acr_id
 }
